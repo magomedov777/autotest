4d78e9457f58f3b4823ca400a8f3095cefd11250
Arkadiy <alibullterrier@gmail.com>
2023-04-12 13:38:38 +0300

    hw2 changed

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -35,7 +35,7 @@ function Affair(props: AffairPropsType) {
             <button
                 id={'hw2-button-delete-' + props.affair._id}
                 className={buttonClass}
-                onClick={() => deleteCallback}
+                onClick={()=> deleteCallback(affairClass)}
             >
                 {/*текст кнопки могут изменить студенты*/}
                 X
