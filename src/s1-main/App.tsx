b5410934f58a0e1d9779f273d360194830360ea9
Arkadiy <alibullterrier@gmail.com>
2023-04-12 18:10:38 +0300

    hw3

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import s from './App.module.css'
-import HW4 from '../s2-homeworks/hw04/HW4'
+// import HW4 from '../s2-homeworks/hw04/HW4'
+import HW3 from '../s2-homeworks/hw03/HW3'
 
 function App() {
     return (
@@ -8,8 +9,8 @@ function App() {
             {/* <HW1/> */}
             {/*раскомментировать по ходу выполнения*/}
             {/* <HW2/> */}
-            {/* <HW3/> */}
-            <HW4/>
+            <HW3/>
+            {/* <HW4/> */}
 
             {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
             {/*<HW5/>*/}
