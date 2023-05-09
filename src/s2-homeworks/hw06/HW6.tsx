646af9760ddc9548aff58eacf8e6b9eb0d301cd1
Arkadiy <alibullterrier@gmail.com>
2023-05-09 07:53:21 +0300

    changed

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -34,9 +34,9 @@ const HW6 = () => {
                         value={value}
                         onChangeText={setValue}
                         spanProps={{
-                            id: 'hw6-editable-span',
-                            defaultText: 'some text new text',
-                            // children: value ? undefined : 'enter text'
+                            // id: 'hw6-editable-span',
+                            // defaultText: 'enter text...',
+                            children: value ? undefined : 'enter text'
                         }}
                     />
                 </div>
