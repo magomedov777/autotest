48d7d54bdadb0f66a421683e93cb092270044eb3
Arkadiy <alibullterrier@gmail.com>
2023-05-14 12:11:43 +0300

    complete hw

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import s from './App.module.css'
 // import HW4 from '../s2-homeworks/hw04/HW4'
 // import HW3 from '../s2-homeworks/hw03/HW3'
-// import HW5 from "../s2-homeworks/hw05/HW5";
-// import HW6 from "../s2-homeworks/hw06/HW6";
+import HW5 from "../s2-homeworks/hw05/HW5";
+import HW6 from "../s2-homeworks/hw06/HW6";
 import HW7 from '../s2-homeworks/hw07/HW7';
 
 function App() {
@@ -16,8 +16,8 @@ function App() {
             {/*<HW4/>*/}
 
             {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
-            {/*<HW5/>*/}
-            {/*<HW6/>*/}
+            <HW5/>
+            <HW6/>
             <HW7/>
         </div>
     )
