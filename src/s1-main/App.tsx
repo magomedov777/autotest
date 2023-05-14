a312c56734b9a70b1b32c8094480231dba4f2e31
Arkadiy <alibullterrier@gmail.com>
2023-05-14 09:43:40 +0300

    hw

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -4,6 +4,7 @@ import s from './App.module.css'
 // import HW3 from '../s2-homeworks/hw03/HW3'
 import HW5 from "../s2-homeworks/hw05/HW5";
 import HW6 from "../s2-homeworks/hw06/HW6";
+import HW7 from '../s2-homeworks/hw07/HW7';
 
 function App() {
     return (
@@ -15,8 +16,9 @@ function App() {
             {/*<HW4/>*/}
 
             {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
-            <HW5/>
-            <HW6/>
+            {/* <HW5/>
+            <HW6/> */}
+            <HW7/>
         </div>
     )
 }
